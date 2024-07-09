e975af75f1e57e935edb632850266351eafeb8ec
blister-pack <andrecrujo@hotmail.com>
2024-07-10 00:03:24 +0200

    exercise with some and every finished

--- 07 - Array Cardio Day 2/script.js
+++ 07 - Array Cardio Day 2/script.js
@@ -15,14 +15,21 @@ const comments = [
     { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
-    // Some and Every Checks
-    // Array.prototype.some() // is at least one person 19 or older?
-    // Array.prototype.every() // is everyone 19 or older?
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
+function over19(person) {
+    return person.year < 2005;
+}
+console.log(people.some(over19));
+console.log(people.every(over19));
+    
 
-    // Array.prototype.find()
-    // Find is like filter, but instead returns just the one you are looking for
-    // find the comment with the ID of 823423
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+comments.find()
 
-    // Array.prototype.findIndex()
-    // Find the comment with this ID
-    // delete the comment with the ID of 823423
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
