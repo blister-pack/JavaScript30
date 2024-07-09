182d8587dd4597b25249334d94d058ece2468495
blister-pack <andrecrujo@hotmail.com>
2024-07-10 00:08:24 +0200

    all the exercises are complete

--- 07 - Array Cardio Day 2/script.js
+++ 07 - Array Cardio Day 2/script.js
@@ -28,8 +28,13 @@ console.log(people.every(over19));
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-comments.find()
+let foundComment = comments.find((comment) => comment.id === 823423);
+console.log(foundComment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+let foundCommentIndex = comments.findIndex((comment) => comment.id === 823423);
+console.log(foundCommentIndex);
+delete comments[foundCommentIndex];
+console.table(comments);
